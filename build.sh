85ac58ec7d3816d5d3bf89d7f5ce722698df7589
Yuto Watanabe <yuto.w51942@gmail.com>
2021-04-03 19:35:53 +0900

    enable docker buildkit

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1
+export DOCKER_BUILDKIT=1
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 tag_name="quick_train"
 
-docker build -t ${tag_name}:latest .
+docker build --progress=tty -t ${tag_name}:latest .
 
 echo ""
 echo "docker run --rm --gpus all -v ${SCRIPT_DIR}/core:/core -v ${SCRIPT_DIR}/tools:/tools -i -t ${tag_name}:latest /bin/bash"
